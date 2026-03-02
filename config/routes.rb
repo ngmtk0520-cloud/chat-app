c0b531a076ab420540972ea84dd66f726c121acd
keisuke nagamatsu <ngmt.k0520@gmail.com>
2026-03-02 14:28:28 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "messages#index" 
   get 'messages/index'
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:edit, :update ]
+  resources :rooms, only: [:new, :create ]
 end
